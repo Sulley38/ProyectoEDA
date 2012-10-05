2a696ab7f16f072cc3991b62ad3851e3c64cfec1
Iván Matellanes <imatellanes003@ikasle.ehu.es>
2012-10-05 09:54:24 +0200

    Pruebas en el main de los cuatro métodos

--- src/gestionDeSentencias/Main.java
+++ src/gestionDeSentencias/Main.java
@@ -1,20 +1,60 @@
 package gestionDeSentencias;
-
 import java.io.*;
 
 public class Main {
 
 	public static void main (String[] args) throws IOException {	
 		Almacen m;
+		String file = "data/A3.txt";
+		// Cargar el almacén
+		System.out.println("Cargando el fichero \"" + file + "\"");
 		long t=System.currentTimeMillis();
-		m=new Almacen("data/A3.txt");
-		System.out.println(System.currentTimeMillis()-t);
-		FileWriter fstream = new FileWriter("data/out.txt");
-		BufferedWriter out = new BufferedWriter(fstream);
+		m=new Almacen(file);
+		System.out.print("Sentencias leídas en ");
+		System.out.print(System.currentTimeMillis()-t);
+		System.out.println(" ms");
+		
+		
+		// Prueba 1
+		BufferedWriter out = new BufferedWriter(new FileWriter("data/out1.txt"));
+		System.out.println("Escribiendo sentencias que tienen el sujeto \"<http://www.Department0.Univ5.edu/UndergraduateStudent500>\"");
+		t=System.currentTimeMillis();
+		m.sentenciasPorSujeto("<http://www.Department0.Univ5.edu/UndergraduateStudent500>", out);
+		System.out.print("Escrito en ");
+		System.out.print(System.currentTimeMillis()-t);
+		System.out.println(" ms");
+		out.close();
+		
+		// Prueba 2
+		out = new BufferedWriter(new FileWriter("data/out2.txt"));
+		System.out.println("Escribiendo sentencias distintas que tienen el sujeto \"<http://www.Department0.Univ5.edu/UndergraduateStudent500>\"");
+		t=System.currentTimeMillis();
+		m.sentenciasDistintasPorSujeto("<http://www.Department0.Univ5.edu/UndergraduateStudent500>", out);
+		System.out.print("Escrito en ");
+		System.out.print(System.currentTimeMillis()-t);
+		System.out.println(" ms");
+		out.close();
+		
+		// Prueba 3
+		out = new BufferedWriter(new FileWriter("data/out3.txt"));
+		System.out.println("Escribiendo propiedades distintas que aparecen en el almacén");
+		t=System.currentTimeMillis();
+		m.propiedadesDistintas(out);
+		System.out.print("Escrito en ");
+		System.out.print(System.currentTimeMillis()-t);
+		System.out.println(" ms");
+		out.close();
+		
+		// Prueba 4
+		out = new BufferedWriter(new FileWriter("data/out4.txt"));
+		System.out.println("Escribiendo entidades distintas que son sujeto y también objeto de alguna sentencia");
 		t=System.currentTimeMillis();
 		m.entidadesSujetoObjeto(out);
-		System.out.println(System.currentTimeMillis()-t);
+		System.out.print("Escrito en ");
+		System.out.print(System.currentTimeMillis()-t);
+		System.out.println(" ms");
 		out.close();
+		
 	}
 	
 }
