0a232569c72a152500324b13ee4366e8155eeb91
Iván Matellanes <imatellanes003@ikasle.ehu.es>
2012-10-17 17:18:03 +0200

    Las pruebas no tienen en cuenta el tiempo en imprimir fichero

--- src/gestionDeSentencias/Main.java
+++ src/gestionDeSentencias/Main.java
@@ -1,12 +1,12 @@
 package gestionDeSentencias;
 
+import estructurasDeDatos.*;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main (String[] args) {	
 		Almacen m;
-		int prueba;
 		Scanner in = new Scanner(System.in);
 		String sujeto = "<http://swat.cse.lehigh.edu/onto/univ-bench.owl#AdministrativeStaff>";
 		String file = "data/in/";
@@ -17,63 +17,76 @@ public class Main {
 		// Cargar el almacén
 		System.out.println("Cargando el fichero \"" + file + "\"");
 		long t;				
-		t=System.currentTimeMillis();
-		m=new Almacen(file);
-		t=System.currentTimeMillis()-t;
+		t = System.currentTimeMillis();
+		m = new Almacen(file);
+		t = System.currentTimeMillis() - t;
 		System.out.print("Sentencias leídas en ");
 		System.out.print(t);
 		System.out.println(" ms");
 		
-		while(true){
-			
+		int respuesta = 1;
+		ListaEnlazada<String> le;
+		ListaArray<String> la;
+		while (respuesta > 0) {
+
 			System.out.println("Escriba el numero de la prueba a realizar:");
-			prueba = in.nextInt();
-			switch(prueba){
+			respuesta = in.nextInt();
+			switch (respuesta) {
 			case 1:
 				// Prueba 1
-				System.out.println("Escribiendo sentencias que tienen el sujeto "+sujeto);
-				t=System.nanoTime();
-				m.sentenciasPorSujeto(sujeto).imprimirEnFichero("data/out/B0.txt");
-				t=System.nanoTime()-t;
+				System.out.println("Escribiendo sentencias que tienen el sujeto " + sujeto);
+				t = System.nanoTime();
+				le = m.sentenciasPorSujeto(sujeto);
+				t = System.nanoTime() - t;
+				le.imprimirEnFichero("data/out/B0.txt");
 				System.out.print("Escrito en ");
-				System.out.print(t/1e6);
-				System.out.println(" ms");
-				break;
-			case 2:	
-				// Prueba 2
-				System.out.println("Escribiendo sentencias distintas que tienen el sujeto "+sujeto);
-				t=System.nanoTime();
-				m.sentenciasDistintasPorSujeto(sujeto).imprimirEnFichero("data/out/B1.txt");
-				t=System.nanoTime()-t;
-				System.out.print("Escrito en ");
-				System.out.print(t/1e6);
+				System.out.print(t / 1e6);
 				System.out.println(" ms");
 				break;
 				
-			case 3:	
+			case 2:
+				// Prueba 2
+				System.out.println("Escribiendo sentencias distintas que tienen el sujeto " + sujeto);
+				t = System.nanoTime();
+				le = m.sentenciasDistintasPorSujeto(sujeto);
+				t = System.nanoTime() - t;
+				le.imprimirEnFichero("data/out/B1.txt");
+				System.out.print("Escrito en ");
+				System.out.print(t / 1e6);
+				System.out.println(" ms");
+				break;
+
+			case 3:
 				// Prueba 3
 				System.out.println("Escribiendo propiedades distintas que aparecen en el almacén");
-				t=System.nanoTime();
-				m.propiedadesDistintas().imprimirEnFichero("data/out/B2.txt");
-				t=System.nanoTime()-t;
+				t = System.nanoTime();
+				la = m.propiedadesDistintas();
+				t = System.nanoTime() - t;
+				la.imprimirEnFichero("data/out/B2.txt");
 				System.out.print("Escrito en ");
-				System.out.print(t/1e6);
+				System.out.print(t / 1e6);
 				System.out.println(" ms");
 				break;
-				
-			case 4:	
+
+			case 4:
 				// Prueba 4
 				System.out.println("Escribiendo entidades distintas que son sujeto y también objeto de alguna sentencia");
-				t=System.nanoTime();
-				m.entidadesSujetoObjeto().imprimirEnFichero("data/out/B3.txt");
-				t=System.nanoTime()-t;
+				t = System.nanoTime();
+				le = m.entidadesSujetoObjeto();
+				t = System.nanoTime() - t;
+				le.imprimirEnFichero("data/out/B3.txt");
 				System.out.print("Escrito en ");
-				System.out.print(t/1e6);
+				System.out.print(t / 1e6);
 				System.out.println(" ms");
+				break;
+
+			case 0:
+				// Salir
+				System.out.println("Fin del programa de pruebas");
 				break;
 				
 			default:
-				System.out.println("Escriba un numero del 1 al 4");				
+				System.out.println("Escriba un número del 1 al 4 para ejecutar las pruebas, o 0 para salir");
 				break;
 			}
 		}
